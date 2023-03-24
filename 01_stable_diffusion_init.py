0044a945da331eec8cfa58327718bd6b60271900
sid2656 <devilzy2656@163.com>
2023-03-24 18:42:03 +0800

    Update 01_stable_diffusion_init.py

--- 01_stable_diffusion_init.py
+++ 01_stable_diffusion_init.py
@@ -1,3 +1,6 @@
 !git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
 %cd /content/stable-diffusion-webui
+%cp /content/colaboratory/scripts/multi_frame_render.py /content/stable-diffusion-webui/scripts
+%cp /content/colaboratory/scripts/multi_frame_rendering.py /content/stable-diffusion-webui/scripts
+%cp /content/colaboratory/scripts/video2frame.py /content/stable-diffusion-webui/scripts
 !python launch.py --share --xformers --enable-insecure-extension-access
