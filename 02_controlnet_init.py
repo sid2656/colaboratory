8873b9f3bdb09d3583a385a860383a2710980ff2
sid2656 <devilzy2656@163.com>
2023-03-24 18:16:37 +0800

    Update 02_controlnet_init.py

--- 02_controlnet_init.py
+++ 02_controlnet_init.py
@@ -1,13 +1,6 @@
 
 %cd /content/stable-diffusion-webui/extensions
 !git clone https://github.com/Mikubill/sd-webui-controlnet
+%cd /content/stable-diffusion-webui
+!python launch.py --share --xformers --enable-insecure-extension-access
 
-%cd /content/stable-diffusion-webui/models/ControlNet
-!wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_canny.pth -O /content/stable-diffusion-webui/models/ControlNet/control_sd15_canny.pth
-!wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_depth.pth -O /content/stable-diffusion-webui/models/ControlNet/control_sd15_depth.pth
-!wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_hed.pth -O /content/stable-diffusion-webui/models/ControlNet/control_sd15_hed.pth
-!wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_mlsd.pth -O /content/stable-diffusion-webui/models/ControlNet/control_sd15_mlsd.pth
-!wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_normal.pth -O /content/stable-diffusion-webui/models/ControlNet/control_sd15_normal.pth
-!wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_openpose.pth -O /content/stable-diffusion-webui/models/ControlNet/control_sd15_openpose.pth
-!wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_scribble.pth -O /content/stable-diffusion-webui/models/ControlNet/control_sd15_scribble.pth
-!wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_seg.pth -O /content/stable-diffusion-webui/models/ControlNet/control_sd15_seg.pth
