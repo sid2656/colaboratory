1dd0a94f319ea6c6a6b2c7df0bb47fc0fdb9b8a5
sid2656 <devilzy2656@163.com>
2023-03-24 18:40:20 +0800

    Update 01_stable_diffusion_init.py

--- 01_stable_diffusion_init.py
+++ 01_stable_diffusion_init.py
@@ -1,4 +1,3 @@
-cd /
 !git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
 %cd /content/stable-diffusion-webui
 !python launch.py --share --xformers --enable-insecure-extension-access
