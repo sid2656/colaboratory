ad8492080c347dc221e9e852a43354b090573738
sid2656 <devilzy2656@163.com>
2023-03-24 17:57:02 +0800

    Create 10_lora_base_model.py

---
+++ 10_lora_base_model.py
@@ -0,0 +1,2 @@
+%cd /content/stable-diffusion-webui/extensions
+!wget https://huggingface.co/kansas/yesmix_v16/resolve/main/yesmix_v16.safetensors -O /content/stable-diffusion-webui/models/Stable-diffusion/yesmix_v16.safetensors
