48e82fa9f6cd43370a9524433d3530a60ffb3b72
sid2656 <devilzy2656@163.com>
2023-03-24 18:19:58 +0800

    Update and rename 10_chilloutmix_NiPrunedFp32Fix_load.py to 10_lora_base_model_chilloutmix_NiPrunedFp32Fix_load.py

--- 10_lora_base_model_chilloutmix_NiPrunedFp32Fix_load.py
+++ 10_lora_base_model_chilloutmix_NiPrunedFp32Fix_load.py
@@ -1,6 +1,4 @@
-
-echo "download chilloutmix model start ..."
 %cd /content/stable-diffusion-webui/extensions
 !wget https://huggingface.co/SakerLy/chilloutmix_NiPrunedFp32Fix/resolve/main/chilloutmix_NiPrunedFp32Fix.safetensors -O /content/stable-diffusion-webui/models/Stable-diffusion/chilloutmix_NiPrunedFp32Fix.safetensors
 !wget https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors -O /content/stable-diffusion-webui/models/VAE/vae-ft-mse-840000-ema-pruned.safetensorsvae-ft-mse-840000-ema-pruned.safetensors
-echo "download chilloutmix model end ..."
+
