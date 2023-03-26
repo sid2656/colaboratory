9f36a923a1a2a0778bbfb086952c3dc836cb815f
sid2656 <devilzy2656@163.com>
2023-03-27 00:03:39 +0800

    Create 03_vae_init.py

---
+++ 03_vae_init.py
@@ -0,0 +1,4 @@
+
+%cd /content/stable-diffusion-webui/models/VAE
+!wget https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors -O /content/stable-diffusion-webui/models/VAE/vae-ft-mse-840000-ema-pruned.safetensors
+
